e83508cacee3fe87ede2211e6b4e74eddb6a9339
xcsoft <email@xcsoft.top>
2021-12-28 16:56:03 +0800

    Edit web

--- Applications/Events.php
+++ Applications/Events.php
@@ -10,7 +10,6 @@ class Events
 {
     public static function onConnect($client_id)
     {
-        Tool::out("$client_id connected");
         $auth_timer_id = Timer::add(10, function ($client_id) {
             Gateway::closeClient($client_id, json_encode(['type' => 'auth_timeout']));
         }, array($client_id), false);
@@ -19,35 +18,39 @@ class Events
 
     public static function onMessage($client_id, $message)
     {
-        $msgData = json_decode($message, true);
+        $recv = json_decode($message, true);
+        $client_ip = $_SERVER['REMOTE_ADDR'] ?? '1.1.1.1';
 
-        if (!$msgData || !isset($msgData['type'])) {
-            Tool::out("$client_id > 非法的请求,断开连接");
+        if (!$recv || !isset($recv['type'])) {
+            Tool::out("$client_id 非法的请求,断开连接");
             Gateway::closeClient($client_id, json_encode(['type' => 'invalid_request']));
             return;
         }
-        $type = $msgData['type'];
-        if ($type !== 'ping') {
-            Tool::out("$client_id > $message"); //TODO debug
+        $type = $recv['type'];
+
+        if (!in_array($type, ['ping', 'server_info', 'login'])) {
+            Tool::out("$client_id $message"); //TODO debug
         }
         switch ($type) {
             case "ping":
                 Gateway::sendToClient($client_id, json_encode(['type' => 'pong']));
                 break;
             case "login":
-                $token = $msgData['token'] ?? '';
-                $platform = $msgData['platform'] ?? '';
+                $token = $recv['token'] ?? '';
+                $platform = $recv['platform'] ?? '';
+
                 if ($platform == "server") {
                     //服务器节点
                     if ($token !== SERVERTOKEN) {
-                        Tool::out("$client_id > 非法的token,断开连接");
+                        Tool::out("$client_id 非法的token,断开连接");
                         Gateway::closeClient($client_id, json_encode(['type' => 'invalid_token']));
                         return;
                     }
+                    $tag = $recv['tag'] ?? 'unKnow Server';
                     $server_info = [
                         'platform' => 'server',
-                        'tag' => $msgData['tag'] ?? 'unKnow Server',
-                        'ip' => $_SERVER['REMOTE_ADDR'] ?? '1.1.1.1',
+                        'tag' => $tag,
+                        'ip' => $client_ip,
                         'online_time' => time(),
                     ];
                     Gateway::joinGroup($client_id, 'server');
@@ -57,12 +60,13 @@ class Events
                         'type' => 'server_online',
                         "data" => $server_info,
                     ]));
+                    Tool::out("${client_id} ${tag} 连接到服务器");
                 } else {
                     //网页
                     Gateway::joinGroup($client_id, 'web');
                     Gateway::updateSession($client_id, [
                         'platform' => 'web',
-                        'ip' => $_SERVER['REMOTE_ADDR'] ?? '1.1.1.1',
+                        'ip' => $client_ip,
                     ]); // 设置连接者信息
 
                     // 发送当前连接者信息
@@ -78,16 +82,17 @@ class Events
                         ]);
                     }
                     Gateway::sendToCurrentClient(json_encode(['type' => 'login_success', 'server_list' => $server_lists]));
+                    //Tool::out("${client_id} (web) 连接到服务器");
                 }
                 Timer::del(Gateway::getSession($client_id)['auth_timer_id']); //删除Timer
                 break;
-            case 'server_base_info': //服务器回传 基本信息
-                $server_base_info = $msgData['data'] ?? [];
-                $server_base_info['tag'] = Gateway::getSession($client_id)['tag'];
-                Gateway::sendToGroup("web", json_encode(['type' => 'server_base_info', 'client_id' => $client_id, 'server_base_info' => $server_base_info]));
+            case 'server_info': //服务器回传 基本信息
+                $server_info = $recv['data'] ?? [];
+                $server_info['tag'] = Gateway::getSession($client_id)['tag'];
+                Gateway::sendToGroup("web", json_encode(['type' => 'server_info', 'client_id' => $client_id, 'server_info' => $server_info]));
                 break;
             default:
-                Tool::out("$client_id > 未知的请求类型");
+                Tool::out("$client_id 未知的请求类型");
         }
     }
 
@@ -95,8 +100,18 @@ class Events
     {
         $client_platform = $_SESSION['platform'] ?? null;
         if ($client_platform == 'server') {
+            $tag = $_SESSION['tag'] ?? null;
+
             GateWay::sendToGroup("web", json_encode(['type' => 'server_offline', 'client_id' => $client_id]));
+            Tool::out("${client_id} ${tag} 失去连接");
         }
-        Tool::out("$client_id > logout");
     }
 }
+
+
+/*
+    三个 Group
+
+    server: 服务器节点
+    web: 网页节点
+*/
--- web/src/index.js
+++ web/src/index.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
-var ws = new WebSocket("ws://127.0.0.1:8282");
-ws.onopen = function () {
-  // Web Socket 已连接上，使用 send() 方法发送数据
-  ws.send(JSON.stringify({ 'type': 'login' }));
-};
+const App = () => {
 
-ws.onmessage = function (evt) {
-  var resData = JSON.parse(evt.data)
-  console.log(resData)
+  const [serverInfo, serServerInfo] = useState(null);
 
-  var type = resData.type
-  if (type === 'login_success') {
-    
+  const establish = () => {
+    console.log("Try to connect to server")
+
+    var ws = new WebSocket("ws://10.11.11.11:8282");
+
+    ws.onopen = function () {
+      ws.send(JSON.stringify({ 'type': 'login' }));
+    };
+
+    ws.onmessage = function (evt) {
+      var resData = JSON.parse(evt.data)
+      console.log(resData)
+
+      var type = resData.type
+      if (type === 'login_success') {
+        console.log('Establish connection')
+        serServerInfo(JSON.stringify(resData.server_list))
+      }
+    };
+
+    ws.onclose = function () {
+      console.log('Lost connection, try to reconnect in 1 second')
+      setTimeout(() => establish(), 1000)
+    };
   }
 
-};
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => establish(), [])
 
-ws.onclose = function () {
-  console.log('closed')
-};
-
-const App = () => {
   return (
-    <h1>HelloWorld</h1>
+    <p>{serverInfo}</p>
   );
 }
 
