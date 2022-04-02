16a2867f7021fc88fd3b9f42c6ad8968d7b29623
xcsoft <email@xcsoft.top>
2022-04-02 23:00:29 +0800

    fix: namspace

--- server/Applications/Events.php
+++ server/Applications/Events.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "/config.php";
 
 use \GatewayWorker\Lib\Gateway;
 use \Workerman\Lib\Timer;
-use \Class\Tool;
+use \Includes\Tool;
 
 class Events
 {
--- server/Applications/start_web.php
+++ server/Applications/start_web.php
@@ -8,7 +8,7 @@ use \Workerman\Worker;
 use \Workerman\Connection\TcpConnection;
 use \Workerman\Protocols\Http\Request;
 use \Workerman\Protocols\Http\Response;
-use \Class\Tool;
+use \Includes\Tool;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 $http_worker = new Worker("http://0.0.0.0:" . WEBPORT);
